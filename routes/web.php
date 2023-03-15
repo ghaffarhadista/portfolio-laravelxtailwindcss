dfa1b28dffb84792eb850263257deb0f0dfffca1
Ghaffar Hadista Hariadi <94441683+ghaffarhadista@users.noreply.github.com>
2023-03-15 10:59:33 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.welcome');
-});
-
-Route::get('/home', function () {
     return view('pages.home');
 });
